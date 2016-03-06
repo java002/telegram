be489b8711ddb94927211258d654a649161e28f7
java002 <javainsta@yandex.ru>
2016-03-06 22:18:29 +0500

    Create set.php

---
+++ set.php
@@ -0,0 +1,19 @@
+<?php
+// Load composer
+require __DIR__ . '/vendor/autoload.php';
+
+$API_KEY = '124559346:AAHto0LB3_lhlK0ZSrC3hQUzZqOCZUBut3A';
+$BOT_NAME = 'Новая империя';
+$hook_url = 'https://github.com/path/to/hook.php';
+try {
+    // Create Telegram API object
+    $telegram = java002\TelegramBot\Telegram($124559346:AAHto0LB3_lhlK0ZSrC3hQUzZqOCZUBut3A, $Новая империя);
+
+    // Set webhook
+    $result = $telegram->setWebHook($https://github.com/path/to/hook.php);
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    echo $e;
+}
